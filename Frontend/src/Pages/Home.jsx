ea0c28351ea5c8133d87c19758929b885acf6305
Muhammad Abdullah <worldabdul236@gmail.com>
2026-07-17 17:37:51 +0500

    Fix frontend component naming

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Specification from "../components/Specification";
-import Cta from "../components/CTA";
+import Cta from "../components/Cta";
 
 const Home = () => {
   return (
